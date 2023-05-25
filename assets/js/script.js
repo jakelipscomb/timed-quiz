c3ee31856dc9f4a8e921ccb345f5b1f94549fa83
jakelipscomb <jlipscombtx95@gmail.com>
2023-05-25 01:22:12 -0500

    changed html and js to display questions by clicking on the radio icon and clicking the submit button, prompting a correct or incorrect statement. after the timer reaches 0, the page is redirected to the high score page.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,101 +1,129 @@
 // Creating an array of question objects
-var quizQuestions = [
+
+let quizQuestions = [
     {
+        number: 1,
         prompt: "Commonly used data types do NOT include:",
         options: ["(1) Strings", "(2) Booleans", "(3) Alerts", "(4) Numbers"],
-        correct: "3"
+        correct: "3",
     },
     {
+        number: 2,
         prompt: "The condition in an if / else statement is enclosed with _______.",
         options: ["(1) Quotes", "(2) Curly brackets", "(3) Parenthesis", "(4) Square brackets"],
-        correct: "2"
+        correct: "2",
     },
     {
+        number: 3,
         prompt: "Arrays in JavaScript can be used to store ________.",
         options: ["(1) Numbers and Strings", "(2) Other Arrays", "(3) Booleans", "(4) All of the Above"],
-        correct: "4"
+        correct: "4",
     },
     {
+        number: 4,
         prompt: "String values must be enclosed within ________ when being assigned to variables.",
         options: ["(1) Commas", "(2) Curly brackets", "(3) Quotes", "(4) Parenthesis"],
-        correct: "3"
+        correct: "3",
     },
     {
+        number: 5,
         prompt: "A very useful tool used during development and debugging for printing content to the debugger is:",
         options: ["(1) JavaScript", "(2) Terminal/Bash", "(3) for Loops", "(4) console.log"],
-        correct: "4"
+        correct: "4",
     },
 ];
 
-var quiz = document.getElementById("quiz");
-var answers = document.getElementsByClassName(".answer");
-var startButton = document.getElementById("start-button");
+// timer goes here
+var startTimer = document.getElementById("timer");
 var timerSpan = document.getElementById("time-left");
+var timeLeft = 60;
 
-var questionElements = document.getElementById("question");
-var question1 = document.getElementById("question1");
-var question2 = document.getElementById("question2");
-var question3 = document.getElementById("question3");
-var question4 = document.getElementById("question4");
-var question5 = document.getElementById("question5");
-
-let beginQuiz = 0;
-var quizScore = 0;
-var timeLeft = 75;
 
 
 
 function startGame() {
-    var currentQuizQuestion = quizQuestions[beginQuiz]
-    questionElements.textContent = currentQuizQuestion.quizQuestions[i]
-    question1.textContent = currentQuizQuestion.first
-    question2.textContent = currentQuizQuestion.second
-    question3.textContent = currentQuizQuestion.third
-    question4.textContent = currentQuizQuestion.fourth
-    question5.textContent = currentQuizQuestion.fifth
-
-
-    var gameInterval = setInterval(() => {
+    var gameInterval = setInterval(function() {
         timerSpan.textContent = timeLeft--;
-        
-        if(!timeLeft) {
-            clearInterval(gameInterval)
+
+        if(timeLeft < 0) {
+            clearInterval(gameInterval);
             window.location.href = "highscore.html";
-
         }
-
     }, 1000);
 }
 
-// //Write a program that will go through the array and ask each question
+// function adjustTime(currentQuestion.correct) {
+//         if (currentQuestion.correct) {
+//             timeLeft += 10;
+//         } else {
+//             timeLeft -= 10;
+//             if (timeLeft < 0) {
+//                 timeLeft = 0;
+//             }
+//         }
+//     }
 
+startTimer.onclick = startGame();
 
-function clickAnswer() {
-    let answerElements
-    answerElements.forEach(answerElements => {
-        if(answerElements.) {
-            answer = answerElements.id
-        }
-    })
-    return answer
+// quiz goes here
+
+let currentQuestionIndex = 0;
+let questionNumberElement = document.getElementById("question-number");
+let questionPromptElement = document.getElementById("question-prompt");
+let optionsListElement = document.getElementById("options-list");
+let submitButton = document.getElementById("submit-button");
+let resultTextElement = document.getElementById("result-text");
+let nextButton = document.getElementById("next-button");
+let questionContainer = document.getElementById("question-container");
+let resultContainer = document.getElementById("result-container");
+
+function displayQuestion(question) {
+    questionNumberElement.textContent = "Question " + question.number;
+    questionPromptElement.textContent = question.prompt;
+    optionsListElement.innerHTML = "";
+
+    for (let i = 0; i < question.options.length; i++) {
+        let listItem = document.createElement("li");
+        let label = document.createElement("label");
+        let option = document.createElement("input");
+        option.type = "radio";
+        option.name = "option";
+        option.value = i + 1;
+        label.appendChild(option);
+        label.appendChild(document.createTextNode(question.options[i]));
+        listItem.appendChild(label);
+        optionsListElement.appendChild(listItem);
+    }
 }
 
+function processAnswer(answer) {
+    let currentQuestion = quizQuestions[currentQuestionIndex];
+    if (answer === currentQuestion.correct) {
+        resultTextElement.textContent = "Correct answer!";
+    } else {
+        resultTextElement.textContent = "Wrong answer. The correct answer is: " + currentQuestion.correct;
+    }
+    resultContainer.style.display = "block";
+}
 
+submitButton.addEventListener("click", function() {
+    let selectedOption = document.querySelector('input[name="option"]:checked');
+    if (selectedOption) {
+        let answer = selectedOption.value;
+        processAnswer(answer);
+    }
+});
 
+nextButton.addEventListener("click", function() {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < quizQuestions.length) {
+        displayQuestion(quizQuestions[currentQuestionIndex]);
+        questionContainer.style.display = "block";
+        resultContainer.style.display = "none";
+    } else {
+        resultTextElement.textContent = "Quiz finished!";
+        window.location.href = "highscore.html";
+    }
+});
 
-
-startButton.onclick = startGame();
-
-
-
-
-// for (var i=0; i < quizQuestions.length; i++) {
-//     var answer = window.prompt(quizQuestions[i].prompt);
-//     if (answer == quizQuestions[i].answer) {
-//         quizScore++;
-//         alert("Correct!");
-//     } else {
-//         alert("Wrong Answer!");
-//     }
-// }
-// alert("you got " + quizScore + "/" + quizQuestions.length);
+displayQuestion(quizQuestions[currentQuestionIndex]);
